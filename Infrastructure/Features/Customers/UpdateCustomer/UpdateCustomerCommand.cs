3717acde8726d6f39dfa0c444f65d96bcb67d5e8
mfon ukobo <coache1234@gmail.com>
2023-06-03 17:45:17 +0100

    add customer endpoints

---
+++ Infrastructure/Features/Customers/UpdateCustomer/UpdateCustomerCommand.cs
@@ -0,0 +1,62 @@
+﻿using Application.Mediator;
+using Domain.Entities;
+using IdentityModel.Client;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Features.Customers.UpdateCustomer
+{
+    public class UpdateCustomerCommand : ICommand<Result>
+    {
+        public UpdateCustomerCommand(Guid customerId, UpdateCustomerRequest payload)
+        {
+            Payload = payload;
+            CustomerId = customerId;
+        }
+
+        public Guid CustomerId { get; set; }
+        public UpdateCustomerRequest Payload { get; set; }
+    }
+
+    public class UpdateCustomerRequest
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+    }
+
+    internal sealed class UpdateCustomerCommandHandler : ICommandHandler<UpdateCustomerCommand, Result>
+    {
+        private readonly UserManager<User> _userManager;
+
+        public UpdateCustomerCommandHandler(UserManager<User> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        public async Task<Result> Handle(UpdateCustomerCommand request, CancellationToken cancellationToken)
+        {
+            var user = await _userManager.FindByIdAsync(request.CustomerId.ToString());
+            if (user is null)
+            {
+                return new Error($"No User found with the Id: {request.CustomerId}");
+            }
+
+            user.FirstName = request.Payload.FirstName;
+            user.LastName = request.Payload.LastName;
+            user.Email = request.Payload.Email;
+
+            var updateuserResult = await _userManager.UpdateAsync(user);
+            if (!updateuserResult.Succeeded)
+            {
+                return new Error(updateuserResult.Errors.Select(x => x.Description));
+            }
+
+            return Result.SUCCESS;
+        }
+    }
+}
---
+++ WebApi/Controllers/CustomersController.cs
@@ -0,0 +1,70 @@
+﻿using Domain;
+using Domain.Entities;
+using Infrastructure.Common;
+using Infrastructure.Features.Customers.CreateCustomer;
+using Infrastructure.Features.Customers.GetCustomers;
+using Infrastructure.Features.Customers.UpdateCustomer;
+using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+
+namespace WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize(Scopes.ReadUsers)]
+    public class CustomersController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public CustomersController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        /// <summary>
+        /// Gets customers
+        /// </summary>
+        /// <param name="query"></param>
+        /// <returns></returns>
+        [HttpGet]
+        [ProducesResponseType(typeof(PagedList<User>), StatusCodes.Status200OK)]
+        public async Task<IActionResult> GetCustomers([FromQuery] GetCustomersQuery query)
+        {
+            var result = await _mediator.Send(query);
+            return Ok(result);
+        }
+
+        /// <summary>
+        /// Creates a customer
+        /// </summary>
+        /// <param name="command"></param>
+        /// <returns></returns>
+        [HttpPost]
+        [Authorize(Scopes.WriteUsers)]
+        [ProducesResponseType(typeof(PagedList<User>), StatusCodes.Status200OK)]
+        public async Task<IActionResult> CreateCustomer(CreateCustomerCommand command)
+        {
+            var result = await _mediator.Send(command);
+            return result.Handle<IActionResult>(Ok, BadRequest);
+        }
+
+        /// <summary>
+        /// Updates a customer
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="request"></param>
+        /// <returns></returns>
+        [HttpPut("{id:guid}")]
+        [Authorize(Scopes.WriteUsers)]
+        [ProducesResponseType(typeof(PagedList<User>), StatusCodes.Status200OK)]
+        public async Task<IActionResult> UpdateCustomer(Guid id, UpdateCustomerRequest request)
+        {
+            var command = new UpdateCustomerCommand(id, request);
+            var result = await _mediator.Send(command);
+            return result.Handle<IActionResult>(NoContent, BadRequest);
+        }
+    }
+}
